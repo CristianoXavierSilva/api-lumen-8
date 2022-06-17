2cff847744c86f1fbb9cc71796ad17fffe3b4729
Cristiano Xavier Silva <cristiano.anime93@gmail.com>
2022-06-17 03:00:31 -0300

    implement(auth): Lógica do middleware da autenticação tag: main-1.0.0

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\User;
-use Illuminate\Support\Facades\Gate;
+use App\Models\Entities\Acessos;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,16 +22,22 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         // Here you may define how you wish users to be authenticated for your Lumen
         // application. The callback which receives the incoming request instance
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+
+            if ($request->header('Authorization')) {
+
+                $key = explode(' ',$request->header('Authorization'));
+                $user = Acessos::where('remember_token', $key[1])->first();
+
+                if (!empty($user)) {
+                    return $user;
+                }
             }
         });
     }
